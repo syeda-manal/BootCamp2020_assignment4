e252124fc37c52c244b09a6be081b11675212ce1
syeda-manal <>
2020-06-13 16:19:15 +0500

    initial comiit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import UpdateDay from './UpdateDay.js';
+import day from './day.jpg';
+import night from './night.jpeg';
 import './App.css';
 
 function App() {
+  let [isNight, setNight]= useState(false);
+  let [image, setImage]= useState(day);
+  let[counter, setCounter]= useState(1);
+  let [temp, setTemp] = useState(25)
+  let alterDN = isNight ? 'Night': 'Day';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= {`App ${alterDN}`}>
+     
+      <center>
+      <h2>Have a Good {isNight ? "Night" : "Morning"}</h2>
+      <img src = {image} height = "300" width = "300" />
+      <br />
+      <button onClick =  {()=>{setNight(!isNight); isNight ? setImage(day): setImage(night)} }> update Day </button>
+      <p>Value of Counter: {counter}</p>
+      <button onClick = {()=> setCounter(++counter)}>Count</button>
+      <p>Temperature is : {temp} Celsuis  </p>
+
+      <button class = "block" onClick = {()=> setTemp(--temp)}> - </button>
+      <span>       </span>
+      <button class = "block" onClick = {()=> setTemp(++temp)}>+</button>
+      <br />
+      </center>
     </div>
   );
 }
---
+++ src/UpdateDay.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function UpdateDay(){
+    return (
+       console.log("hi from updateDay")
+
+    );
+}
